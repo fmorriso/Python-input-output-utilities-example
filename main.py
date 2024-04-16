599370e25fd4818fcf7d2dd8f7e5cc5a8ed1b7d5
Frederick Morrison <frederick.morrison@outlook.com>
2024-04-16 16:20:29 +0000

    trim unused code

--- main.py
+++ main.py
@@ -4,17 +4,9 @@ import sys
 from PyQt6.QtWidgets import *
 # from PyQt6.QtCore import *
 
-from main_window import MainWindow
-
 
 def get_python_version() -> str:
     return f"{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}"
-
-
-def create_and_display_gui():
-    app = QApplication(sys.argv)
-    w: MainWindow = MainWindow(get_python_version())
-    app.exec()
 
 
 if __name__ == '__main__':
