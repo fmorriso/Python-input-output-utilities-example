476236441c104889efdb3cfbd9f56a432125b6d0
Frederick Morrison <frederick.morrison@outlook.com>
2024-04-16 20:40:39 -0600

    document each method

--- input_utilities.py
+++ input_utilities.py
@@ -4,12 +4,15 @@ from decimal import Decimal
 
 from PyQt6 import QtWidgets
 from PyQt6.QtWidgets import QApplication, QMessageBox
+
+
 # from PyQt6.QtWidgets.QDialogButtonBox import StandardButton
 
 
 class InputUtils:
     @staticmethod
     def get_whole_number(title: str, msg: str, parent=None) -> int:
+        """get a whole number as directed by the specified message"""
         app = QApplication(sys.argv)
         waitingForValidInput = True
         # trap user in dialog until they enter a valid value and click OK
@@ -29,6 +32,7 @@ class InputUtils:
 
     @staticmethod
     def get_decimal_number(title: str, msg: str, parent=None) -> decimal:
+        """return a decimal number as directed by the message"""
         app = QApplication(sys.argv)
         waitingForValidInput = True
         # trap user in dialog until they enter a valid value and click OK
@@ -38,7 +42,8 @@ class InputUtils:
             min = 0
             max = decimal.MAX_EMAX
             decimals = sys.float_info.dig
-            response = QtWidgets.QInputDialog.getDouble(parent, msg, title, 0, min, max, decimals)            # print(f'{response}=')
+            response = QtWidgets.QInputDialog.getDouble(parent, msg, title, 0, min, max,
+                                                        decimals)  # print(f'{response}=')
             if response[1]:
                 waitingForValidInput = False
 
@@ -49,6 +54,7 @@ class InputUtils:
 
     @staticmethod
     def get_yesno_response(question: str, title: str, parent=None) -> bool:
+        """get a yes/no (True/False) response to a question"""
         app = QApplication(sys.argv)
 
         msgBox = QMessageBox()
@@ -63,16 +69,9 @@ class InputUtils:
         app.exit()
         return ret == QMessageBox.StandardButton.Yes
 
-    """
-    items = ["Spring", "Summer", "Fall", "Winter"]
-
-    item, ok = QInputDialog().getItem(self, "QInputDialog().getItem()",
-                                     "Season:", items, 0, False)
-    if ok and not item.isEmpty():
-        itemLabel.setText(item)
-    """
     @staticmethod
-    def get_single_choice(title: str, msg: str, choices: list[str], parent=None) :
+    def get_single_choice(title: str, msg: str, choices: list[str], parent=None):
+        """get a single choide from a list of choices"""
         app = QApplication(sys.argv)
         # flags
         # force user to choose one of the available choices before returning
